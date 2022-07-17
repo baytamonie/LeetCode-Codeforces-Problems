bd1f6c7ec68af25138789163855d913ad78759dc
Mohammad Betamony <51293050+baytamonie@users.noreply.github.com>
2022-07-18 02:10:24 +0300

    Solved an easy question

---
+++ Solutions/src/easy/IsomorphicStrings.java
@@ -0,0 +1,38 @@
+package easy;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.HashMap;
+
+public class IsomorphicStrings {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String s = "badc";
+        String t = "baba";
+        System.out.println(solution.isIsomorphic(s,t));
+
+    }
+    static class Solution{
+        public boolean isIsomorphic(String s, String t) {
+            HashMap<Character,Character> hashMap = new HashMap<Character, Character>();
+            HashMap<Character, Boolean> characterHashMap = new HashMap<Character, Boolean>();
+            for(int i = 0;i<s.length();i++){
+                if(hashMap.containsKey(s.charAt(i))){
+                    if(hashMap.get(s.charAt(i)).equals(t.charAt(i)))
+                        continue;
+                    else
+                        return false;
+                }
+                else{
+                    if(characterHashMap.containsKey(t.charAt(i)))
+                        return false;
+                    hashMap.put(s.charAt(i),t.charAt(i));
+                    characterHashMap.put(t.charAt(i),true);
+                }
+            }
+            return true;
+        }
+    }
+
+}
